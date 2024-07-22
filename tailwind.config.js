966b19290836c19712f0e9db6f71a3daddd3a366
Farzad Heydari <fheydari207@gmail.com>
2024-07-22 13:02:32 +0330

    fix index html page #4325

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         'yellow': '#fff603',
         'textDark': '#231f20',
         'textLight': '#575757',
-        'light': '#f2f2f2',
+        'light': '#F5F5F5',
       },
     },
     container: {
@@ -33,6 +33,16 @@ module.exports = {
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
     },
+    fontSize: {
+      'xs':['9px','14px'],
+      'sm': ['11px', '18px'],
+      'base': ['13px', '22px'],
+      'lg': ['17px', '26px'],
+      'xl': ['21px', '30px'],
+      '2xl': ['23px', '32px'],
+      '3xl': ['27px', '36px'],
+      '4xl': ['33px', '38px'],
+    }
   },
   plugins: [
     require('flowbite/plugin'),
